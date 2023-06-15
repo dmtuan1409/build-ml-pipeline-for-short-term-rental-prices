52d444960998b41aed15e7ffbae498955c29f11d
dmtuan1409 <thichhocchui1999@gmail.com>
2023-06-15 20:25:18 +0700

    commit code fixed

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,8 +29,8 @@ def go(args):
     logger.info('Drop outlier and save the results to a CSV')
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
-    # idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    # df = df[idx].copy()
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
     df.to_csv("clean_sample.csv", index=False)
     
     #Upload file csv to WanDB
